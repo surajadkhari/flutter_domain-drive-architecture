044ecd979477aa6e8ea5b38700ad7d317148ecfa
unknown <surajadkhari@gmail.com>
2022-06-06 19:19:17 +0545

    create post_detail_controller

--- lib/home/data/repository/post_repository.dart
+++ lib/home/data/repository/post_repository.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_architecture/core/error.dart';
+import 'package:flutter_architecture/home/data/data_model/detail_post_model.dart';
 import 'package:flutter_architecture/home/data/data_model/post_model.dart';
 import 'package:flutter_architecture/home/data/data_source/post_data_source.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -11,6 +12,7 @@ final postrepositoryProvider = Provider<Postrepository>((ref) {
 
 abstract class Postrepository {
   Future<Either<AppError, List<PostModel>>> getPost();
+  Future<Either<AppError, PostDetail>> getPostDetail(String id);
 }
 
 class PostrepositoryImp extends Postrepository {
@@ -20,6 +22,16 @@ class PostrepositoryImp extends Postrepository {
   Future<Either<AppError, List<PostModel>>> getPost() async {
     try {
       var result = await _postdatasource.getPost();
+      return Right(result);
+    } on DioError catch (e) {
+      return Left(AppError(message: e.message));
+    }
+  }
+
+  @override
+  Future<Either<AppError, PostDetail>> getPostDetail(String id) async {
+    try {
+      var result = await _postdatasource.getPostDetail(id);
       return Right(result);
     } on DioError catch (e) {
       return Left(AppError(message: e.message));
