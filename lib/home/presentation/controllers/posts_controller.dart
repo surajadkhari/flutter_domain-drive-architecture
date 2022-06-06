a7c5c7a8e78aeda265d9c4c656fd48731811c6a4
unknown <surajadkhari@gmail.com>
2022-06-06 18:18:38 +0545

    complete api integration with Statenotifer with architecture

--- lib/home/presentation/controllers/posts_controller.dart
+++ lib/home/presentation/controllers/posts_controller.dart
@@ -1,10 +1,25 @@
-import 'package:dartz/dartz.dart';
-import 'package:flutter_architecture/core/error.dart';
 import 'package:flutter_architecture/home/data/data_model/post_model.dart';
 import 'package:flutter_architecture/home/data/repository/post_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final getPostProvider =
-    FutureProvider<Either<AppError, List<PostModel>>>((ref) async {
-  return ref.watch(postrepositoryProvider).getPost();
+// final getPostProvider =
+//     FutureProvider<Either<AppError, List<PostModel>>>((ref) async {
+//   return ref.watch(postrepositoryProvider).getPost();
+// });
+
+final postNotifierProvider =
+    StateNotifierProvider<PostNotifier, AsyncValue<List<PostModel>>>((ref) {
+  return PostNotifier(ref.watch(postrepositoryProvider));
 });
+
+class PostNotifier extends StateNotifier<AsyncValue<List<PostModel>>> {
+  PostNotifier(this._postrepository) : super(const AsyncValue.loading()) {
+    fetch();
+  }
+  final Postrepository _postrepository;
+  fetch() async {
+    final result = await _postrepository.getPost();
+    result.fold((l) => state = AsyncValue.error(l.message),
+        ((r) => state = AsyncValue.data(r)));
+  }
+}
--- lib/home/presentation/widgets/home_page.dart
+++ lib/home/presentation/widgets/home_page.dart
@@ -7,30 +7,40 @@ class Homepage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final data = ref.watch(getPostProvider);
+    final data = ref.watch(postNotifierProvider);
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Ftech Api with Arch"),
+        title: const Text("Fetch API with StateNotifier with Architecture"),
       ),
       body: RefreshIndicator(
         onRefresh: (() async {
-          ref.refresh(getPostProvider);
+          ref.refresh(postNotifierProvider);
         }),
         child: data.when(
             data: (result) {
-              return result.fold((l) => Text(l.message), (r) {
-                return SingleChildScrollView(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      ...r.map((e) => ListTile(
-                            title: Text(e.title.toUpperCase()),
-                            subtitle: Text(e.body),
-                          ))
-                    ],
-                  ),
-                );
-              });
+              return SingleChildScrollView(
+                child: Column(
+                  children: [
+                    ...result.map((e) => ListTile(
+                          title: Text(e.title.toUpperCase()),
+                          subtitle: Text(e.body),
+                        ))
+                  ],
+                ),
+              );
+              // return result.fold((l) => Text(l.message), (r) {
+              //   return SingleChildScrollView(
+              //     child: Column(
+              //       mainAxisAlignment: MainAxisAlignment.start,
+              //       children: [
+              //         ...r.map((e) => ListTile(
+              //               title: Text(e.title.toUpperCase()),
+              //               subtitle: Text(e.body),
+              //             ))
+              //       ],
+              //     ),
+              //   );
+              // });
             },
             error: (e, s) => Text(e.toString()),
             loading: () => const Center(child: CircularProgressIndicator())),
