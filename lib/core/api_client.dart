63f5775a313a6d73a771026a2ad3f6d63968bddb
unknown <surajadkhari@gmail.com>
2022-06-06 13:47:39 +0545

    update diopost

--- lib/core/api_client.dart
+++ lib/core/api_client.dart
@@ -2,21 +2,17 @@ import 'package:dio/dio.dart';
 import 'package:flutter_architecture/core/api_constant.dart';
 
 class ApiClient {
-  Future request({required String path, String type = "get"}) async {
-    // final response = await Dio(
-    //   BaseOptions(baseUrl: ApiConstant.baseUrl),
-    // ).get(path);
-
+  Future request(
+      {required String path,
+      String type = "get",
+      required Map<String, dynamic> data}) async {
+    Dio dio = Dio(
+      BaseOptions(baseUrl: ApiConstant.baseUrl),
+    );
     try {
       final response = type == 'get'
-          ? await Dio(
-              BaseOptions(baseUrl: ApiConstant.baseUrl),
-            ).get(path)
-          : type == "resgister"
-              ? await Dio(BaseOptions(baseUrl: ApiConstant.baseUrl)).post(path)
-              : await Dio(
-                  BaseOptions(baseUrl: ApiConstant.baseUrl),
-                ).post(path);
+          ? await dio.get(path)
+          : await dio.post(path, data: data);
       response.data;
     } on DioError catch (e) {
       throw Exception(e);
