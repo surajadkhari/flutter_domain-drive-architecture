87f9661ec97c70ff206f6b32ee58cb5dfc29129f
unknown <surajadkhari@gmail.com>
2022-06-06 18:56:15 +0545

    complete

--- lib/home/presentation/widgets/home_page.dart
+++ lib/home/presentation/widgets/home_page.dart
@@ -7,32 +7,45 @@ class Homepage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final data = ref.watch(getPostProvider);
+    final data = ref.watch(postNotifierProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Ftech API with Architeture"),
       ),
       body: RefreshIndicator(
         onRefresh: (() async {
-          ref.refresh(getPostProvider);
+          ref.refresh(postNotifierProvider);
         }),
         child: data.when(
             data: (result) {
-              return result.fold((l) => Text(l.message), (r) {
-                return SingleChildScrollView(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      ...r.map((e) => Card(
-                            child: ListTile(
-                              title: Text(e.title),
-                              subtitle: Text(e.body),
-                            ),
-                          ))
-                    ],
-                  ),
-                );
-              });
+              return SingleChildScrollView(
+                child: Column(
+                  children: [
+                    ...result.map((e) => Card(
+                          child: ListTile(
+                            title: Text(e.title),
+                            subtitle: Text(e.body),
+                          ),
+                        ))
+                  ],
+                ),
+              );
+
+              // return result.fold((l) => Text(l.message), (r) {
+              //   return SingleChildScrollView(
+              //     child: Column(
+              //       mainAxisAlignment: MainAxisAlignment.start,
+              //       children: [
+              //         ...r.map((e) => Card(
+              //               child: ListTile(
+              //                 title: Text(e.title),
+              //                 subtitle: Text(e.body),
+              //               ),
+              //             ))
+              //       ],
+              //     ),
+              //   );
+              // }
             },
             error: (e, s) => Text(e.toString()),
             loading: () => const Center(child: CircularProgressIndicator())),
