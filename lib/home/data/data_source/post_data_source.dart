7ee05e7b9356be7265a0a700c926293be71b219f
unknown <surajadkhari@gmail.com>
2022-06-06 17:28:53 +0545

    crate postdata provider

--- lib/home/data/data_source/post_data_source.dart
+++ lib/home/data/data_source/post_data_source.dart
@@ -1,15 +1,22 @@
 import 'package:flutter_architecture/core/api_client.dart';
 import 'package:flutter_architecture/core/api_constant.dart';
 import 'package:flutter_architecture/home/data/data_model/post_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final PostdatasourceProvider = Provider<Postdatasource>((ref) {
+  return PostdatasourceImp(ref.watch(apiClientProvider));
+});
 
 abstract class Postdatasource {
   Future<List<PostModel>> getPost();
 }
 
 class PostdatasourceImp extends Postdatasource {
+  final ApiClient _apiClient;
+  PostdatasourceImp(this._apiClient);
   @override
   Future<List<PostModel>> getPost() async {
-    List result = await ApiClient().request(path: ApiConstant.post);
+    List result = await _apiClient.request(path: ApiConstant.post);
 
     return result.map((e) => PostModel.fromJson(e)).toList();
   }
