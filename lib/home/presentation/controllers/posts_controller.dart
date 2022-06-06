b2eb133dfdaa57d2f40a36b96e79c92dc9a2a7a7
unknown <surajadkhari@gmail.com>
2022-06-06 16:36:51 +0545

    create post_controller with future_provider

---
+++ lib/home/presentation/controllers/posts_controller.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_architecture/core/error.dart';
+import 'package:flutter_architecture/home/data/data_model/post_model.dart';
+import 'package:flutter_architecture/home/data/repository/post_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final getPostProvider =
+    FutureProvider<Either<AppError, List<PostModel>>>((ref) async {
+  return PostrepositoryImp().getPost();
+});
