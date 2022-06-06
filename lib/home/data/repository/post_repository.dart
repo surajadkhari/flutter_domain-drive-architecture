ef5ea1e480d54fc8be8022caad2cbdca33ee9175
unknown <surajadkhari@gmail.com>
2022-06-06 17:35:37 +0545

    create post_data_source_provider

--- lib/home/data/repository/post_repository.dart
+++ lib/home/data/repository/post_repository.dart
@@ -3,16 +3,23 @@ import 'package:dio/dio.dart';
 import 'package:flutter_architecture/core/error.dart';
 import 'package:flutter_architecture/home/data/data_model/post_model.dart';
 import 'package:flutter_architecture/home/data/data_source/post_data_source.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final postrepositoryProvider = Provider<Postrepository>((ref) {
+  return PostrepositoryImp(ref.watch(PostdatasourceProvider));
+});
 
 abstract class Postrepository {
   Future<Either<AppError, List<PostModel>>> getPost();
 }
 
 class PostrepositoryImp extends Postrepository {
+  final Postdatasource _postdatasource;
+  PostrepositoryImp(this._postdatasource);
   @override
   Future<Either<AppError, List<PostModel>>> getPost() async {
     try {
-      var result = await PostdatasourceImp().getPost();
+      var result = await _postdatasource.getPost();
       return Right(result);
     } on DioError catch (e) {
       return Left(AppError(message: e.message));
