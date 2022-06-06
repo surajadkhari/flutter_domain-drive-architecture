017dca18cbad3b13fb055895f2c2b58996032e94
unknown <surajadkhari@gmail.com>
2022-06-06 18:22:57 +0545

    complete api fetch with future_provider with architecture

--- lib/home/presentation/widgets/home_page.dart
+++ lib/home/presentation/widgets/home_page.dart
@@ -7,40 +7,30 @@ class Homepage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final data = ref.watch(postNotifierProvider);
+    final data = ref.watch(getPostProvider);
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Fetch API with StateNotifier with Architecture"),
+        title: const Text("Ftech API with Architeture"),
       ),
       body: RefreshIndicator(
         onRefresh: (() async {
-          ref.refresh(postNotifierProvider);
+          ref.refresh(getPostProvider);
         }),
         child: data.when(
             data: (result) {
-              return SingleChildScrollView(
-                child: Column(
-                  children: [
-                    ...result.map((e) => ListTile(
-                          title: Text(e.title.toUpperCase()),
-                          subtitle: Text(e.body),
-                        ))
-                  ],
-                ),
-              );
-              // return result.fold((l) => Text(l.message), (r) {
-              //   return SingleChildScrollView(
-              //     child: Column(
-              //       mainAxisAlignment: MainAxisAlignment.start,
-              //       children: [
-              //         ...r.map((e) => ListTile(
-              //               title: Text(e.title.toUpperCase()),
-              //               subtitle: Text(e.body),
-              //             ))
-              //       ],
-              //     ),
-              //   );
-              // });
+              return result.fold((l) => Text(l.message), (r) {
+                return SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      ...r.map((e) => ListTile(
+                            title: Text(e.title),
+                            subtitle: Text(e.body),
+                          ))
+                    ],
+                  ),
+                );
+              });
             },
             error: (e, s) => Text(e.toString()),
             loading: () => const Center(child: CircularProgressIndicator())),
