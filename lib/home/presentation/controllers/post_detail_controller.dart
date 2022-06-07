e7920701b1e1e7d430b721f8a87129e58d97a180
unknown <surajadkhari@gmail.com>
2022-06-07 07:42:56 +0545

    post detail controller

---
+++ lib/home/presentation/controllers/post_detail_controller.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_architecture/home/data/data_model/post_detail_model.dart';
+import 'package:flutter_architecture/home/data/repository/post_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class PostDetailNotifier extends StateNotifier<AsyncValue<PostDetailModel>> {
+  PostDetailNotifier(this._postrepository, this.id)
+      : super(const AsyncValue.loading()) {
+    fetch();
+  }
+  final Postrepository _postrepository;
+  final int id;
+  fetch() async {
+    final result = await _postrepository.getPostDetail(id);
+    result.fold(
+        (l) => state = AsyncValue.error(l.message), (r) => AsyncValue.data(r));
+  }
+}
+
+final postDetailNotifierProvider = StateNotifierProvider.family<
+    PostDetailNotifier, AsyncValue<PostDetailModel>, int>((ref, id) {
+  return PostDetailNotifier(ref.watch(postrepositoryProvider), id);
+});
